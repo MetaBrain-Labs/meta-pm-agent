40b02db7f68e7cc8f8396fafada4253b80e9d539
nightmare <lfq2376939781@gmail.com>
2026-05-26 18:46:51 +0800

    init - 初始化项目结构

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello API",
+  });
+});
+
+serve({
+  fetch: app.fetch,
+  port: 3001,
+});
